14272145e807ef05b6cdec5766562fa37047c448
Acasm94 <acasm94@gmail.com>
2018-01-12 20:29:01 +0100

    First commit

---
+++ AuthorizationProviderServer/src/main/java/com/sep/AuthorizationProviderServer/AuthorizationProviderServerApplication.java
@@ -0,0 +1,12 @@
+package com.sep.AuthorizationProviderServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationProviderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationProviderServerApplication.class, args);
+	}
+}
---
+++ AuthorizationProviderServer/src/test/java/com/sep/AuthorizationProviderServer/AuthorizationProviderServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sep.AuthorizationProviderServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthorizationProviderServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
