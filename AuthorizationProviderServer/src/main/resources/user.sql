80e39775383697a017820f3e1af523b69333d0a8
mirkomikac <mmikac27@gmail.com>
2018-01-28 13:36:55 +0100

    Reduced amount of privileges for salesperson and added administrator.

--- AuthorizationProviderServer/src/main/resources/user.sql
+++ AuthorizationProviderServer/src/main/resources/user.sql
@@ -2,31 +2,36 @@
 insert into user(id, email, name, password, surname) values (1, 'b@b.com', 'B', 'b', 'Bb'); -- Prodavac
 insert into user(id, email, name, password, surname) values (2, 'c@c.com', 'C', 'c', 'Cc'); -- Finansijski analiticar
 
-insert into user(id, email, name, password, surname) values (3, 'sbk@uniqa.com', 'Sundjer Bob', 'Sundjerbob1!', 'Kockalone'); -- Finansijski analiticar
-insert into user(id, email, name, password, surname) values (4, 'pz@uniqa.com', 'Patrik', 'Patrik1!', 'Zvezda'); -- Finansijski analiticar
-insert into user(id, email, name, password, surname) values (5, 'lp@uniqa.com', 'Lignjoslav', 'Lignjoslav1!', 'Pipak'); -- Finansijski analiticar
-insert into user(id, email, name, password, surname) values (6, 'ek@uniqa.com', 'Evgenije', 'Evgenije1!', 'Kraba'); -- Finansijski analiticar
-insert into user(id, email, name, password, surname) values (7, 'so@uniqa.com', 'Sendi', 'Sendio1!', 'Obrascic'); -- Finansijski analiticar
-insert into user(id, email, name, password, surname) values (8, 'sp@uniqa.com', 'Seldon', 'Plankton1!', 'sp@uniqu.com'); -- Prodavac
-insert into user(id, email, name, password, surname) values (9, 'uh@uniqa.com', 'Ukleti', 'Holandjanin1!', 'Holandjanin'); -- Prodavac
-insert into user(id, email, name, password, surname) values (10, 'gp@uniqa.com', 'Gospodja', 'Gpufna1!', 'Pufna'); -- Prodavac
+insert into user(id, email, name, password, surname) values (3, 'sbk@uniqu.com', 'Sundjer Bob', 'Sundjerbob1!', 'Kockalone'); -- Finansijski analiticar
+insert into user(id, email, name, password, surname) values (4, 'pz@uniqu.com', 'Patrik', 'Patrik1!', 'Zvezda'); -- Finansijski analiticar
+insert into user(id, email, name, password, surname) values (5, 'lp@uniqu.com', 'Lignjoslav', 'Lignjoslav1!', 'Pipak'); -- Finansijski analiticar
+insert into user(id, email, name, password, surname) values (6, 'ek@uniqu.com', 'Evgenije', 'Evgenije1!', 'Kraba'); -- Finansijski analiticar
+insert into user(id, email, name, password, surname) values (7, 'so@uniqu.com', 'Sendi', 'Sendio1!', 'Obrascic'); -- Finansijski analiticar
+insert into user(id, email, name, password, surname) values (8, 'sp@uniqu.com', 'Seldon', 'Plankton1!', 'sp@uniqu.com'); -- Prodavac
+insert into user(id, email, name, password, surname) values (9, 'uh@uniqu.com', 'Ukleti', 'Holandjanin1!', 'Holandjanin'); -- Prodavac
+insert into user(id, email, name, password, surname) values (10, 'gp@uniqu.com', 'Gospodja', 'Gpufna1!', 'Pufna'); -- Prodavac
+
+insert into user(id, email, name, password, surname) values (11, 'admin@uniqu.com', 'admin', 'admin', 'admin'); -- Admin
 
 -- Role
 insert into role(id, name) values (1, 'Prodavac');
 insert into role(id, name) values (2, 'FinansijskiAnaliticar');
+insert into role(id, name) values (3, 'Admin');
 
 insert into users_roles (user_id, role_id) values (1, 1);
 insert into users_roles (user_id, role_id) values (2, 2);
-
-insert into users_roles (user_id, role_id) values (8, 1);
-insert into users_roles (user_id, role_id) values (9, 1);
-insert into users_roles (user_id, role_id) values (10, 1);
 
 insert into users_roles (user_id, role_id) values (3, 2);
 insert into users_roles (user_id, role_id) values (4, 2);
 insert into users_roles (user_id, role_id) values (5, 2);
 insert into users_roles (user_id, role_id) values (6, 2);
 insert into users_roles (user_id, role_id) values (7, 2);
+
+insert into users_roles (user_id, role_id) values (8, 1);
+insert into users_roles (user_id, role_id) values (9, 1);
+insert into users_roles (user_id, role_id) values (10, 1);
+
+insert into users_roles (user_id, role_id) values (11, 3);
 
 -- PRIVILEGIJE: 
 
@@ -62,22 +67,7 @@ insert into users_roles (user_id, role_id) values (7, 2);
 		insert into privilege(id, name) values (18, 'getFajlove');
 		insert into privilege(id, name) values (19, 'getSadrzajPravila');
 		insert into privilege(id, name) values (20, 'sacuvajPravilo');
-
-	insert into roles_privileges(role_id, privilege_id) values(1, 1);
-	insert into roles_privileges(role_id, privilege_id) values(1, 2);
-	insert into roles_privileges(role_id, privilege_id) values(1, 3);
-	insert into roles_privileges(role_id, privilege_id) values(1, 4);
-	insert into roles_privileges(role_id, privilege_id) values(1, 5);
-	insert into roles_privileges(role_id, privilege_id) values(1, 6);
-	insert into roles_privileges(role_id, privilege_id) values(1, 7);
-	insert into roles_privileges(role_id, privilege_id) values(1, 8);
-	insert into roles_privileges(role_id, privilege_id) values(1, 9);
-	insert into roles_privileges(role_id, privilege_id) values(1, 10);
-	insert into roles_privileges(role_id, privilege_id) values(1, 11);
-	insert into roles_privileges(role_id, privilege_id) values(1, 12);
-	insert into roles_privileges(role_id, privilege_id) values(1, 13);
-	insert into roles_privileges(role_id, privilege_id) values(1, 14);
-	insert into roles_privileges(role_id, privilege_id) values(1, 15);
+	
 	
 	insert into roles_privileges(role_id, privilege_id) values(2, 1);
 	insert into roles_privileges(role_id, privilege_id) values(2, 2);
@@ -99,6 +89,28 @@ insert into users_roles (user_id, role_id) values (7, 2);
 	insert into roles_privileges(role_id, privilege_id) values(2, 18);
 	insert into roles_privileges(role_id, privilege_id) values(2, 19);
 	insert into roles_privileges(role_id, privilege_id) values(2, 20);
+	
+	
+	insert into roles_privileges(role_id, privilege_id) values(3, 1);
+	insert into roles_privileges(role_id, privilege_id) values(3, 2);
+	insert into roles_privileges(role_id, privilege_id) values(3, 3);
+	insert into roles_privileges(role_id, privilege_id) values(3, 4);
+	insert into roles_privileges(role_id, privilege_id) values(3, 5);
+	insert into roles_privileges(role_id, privilege_id) values(3, 6);
+	insert into roles_privileges(role_id, privilege_id) values(3, 7);
+	insert into roles_privileges(role_id, privilege_id) values(3, 8);
+	insert into roles_privileges(role_id, privilege_id) values(3, 9);
+	insert into roles_privileges(role_id, privilege_id) values(3, 10);
+	insert into roles_privileges(role_id, privilege_id) values(3, 11);
+	insert into roles_privileges(role_id, privilege_id) values(3, 12);
+	insert into roles_privileges(role_id, privilege_id) values(3, 13);
+	insert into roles_privileges(role_id, privilege_id) values(3, 14);
+	insert into roles_privileges(role_id, privilege_id) values(3, 15);
+	insert into roles_privileges(role_id, privilege_id) values(3, 16);
+	insert into roles_privileges(role_id, privilege_id) values(3, 17);
+	insert into roles_privileges(role_id, privilege_id) values(3, 18);
+	insert into roles_privileges(role_id, privilege_id) values(3, 19);
+	insert into roles_privileges(role_id, privilege_id) values(3, 20);
 
 	
 	--InsurancePointOfSale:
@@ -136,5 +148,13 @@ insert into users_roles (user_id, role_id) values (7, 2);
 	insert into roles_privileges(role_id, privilege_id) values(1, 28);
 	insert into roles_privileges(role_id, privilege_id) values(1, 29);
 	
-	
+	insert into roles_privileges(role_id, privilege_id) values(3, 21);
+	insert into roles_privileges(role_id, privilege_id) values(3, 22);
+	insert into roles_privileges(role_id, privilege_id) values(3, 23);
+	insert into roles_privileges(role_id, privilege_id) values(3, 24);
+	insert into roles_privileges(role_id, privilege_id) values(3, 25);
+	insert into roles_privileges(role_id, privilege_id) values(3, 26);
+	insert into roles_privileges(role_id, privilege_id) values(3, 27);
+	insert into roles_privileges(role_id, privilege_id) values(3, 28);
+	insert into roles_privileges(role_id, privilege_id) values(3, 29);
 	
